0b722c3b07b1b945ffb3a07bad4c4ab94139e4be
Max <m.lits@stag.technofuturtic.education>
2026-03-13 16:10:55 +0100

    Ajout d'un formulaire avec le javascript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let formDiv = document.getElementById("formDiv");
+let form = document.createElement("form");
+let inputEmail = document.createElement("input");
+// inputEmail.type =
+let inputMdp = document.createElement("input");
+
+formDiv.appendChild(form);
